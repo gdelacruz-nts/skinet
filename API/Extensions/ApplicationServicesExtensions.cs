93b1e2da5a0395af1d4721cbdf0f6f463aa5e91a
Gary <gdelacruz@neural.com.do>
2023-09-13 10:29:42 -0400

    End of Section 13 V.2

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Errors;
 using Core.Interfaces;
 using Infrastructure.Data;
--- Core/Interfaces/IBasketRepository.cs
+++ Core/Interfaces/IBasketRepository.cs
@@ -5,7 +5,7 @@ namespace Core.Interfaces
     public interface IBasketRepository
     {
         Task<CustomerBasket> GetBasketAsync(string BasketId);
-        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket BasketId);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
         Task<bool> DeleteBasketAsync(string BasketId);
     }
 }
