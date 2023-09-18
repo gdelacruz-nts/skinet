9f8f1beae3ce7e1d609b05d1d90dbd72b4251605
Gary <gdelacruz@neural.com.do>
2023-09-18 16:53:15 -0400

    Half of Section 15

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
+using Core.Entities.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API
@@ -16,6 +19,11 @@ namespace API
                     var context = services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
                     await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                    var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+                    await identityContext.Database.MigrateAsync();
+                    await AppIdentityDbContextSeed.SeedUserAsync(userManager);
                 }catch(Exception ex){
                     var logger = loggerFactory.CreateLogger<Program>();
                     logger.LogError(ex, "An error ocurred during migration");
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 
@@ -9,22 +10,22 @@ namespace API
 {
     public class Startup
     {
-
         private readonly IConfiguration _config;
         public Startup(IConfiguration config)
         {
             _config = config;
         }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<AppIdentityDbContext>( x => {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            });   
 
             services.AddSingleton<IConnectionMultiplexer>(c => {
                 var configuration = ConfigurationOptions.Parse(_config
@@ -33,6 +34,7 @@ namespace API
             });
 
             services.AddApplicationServices();
+            services.AddIdentityService();
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>
             {
