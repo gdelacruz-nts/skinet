1cefd4e2cc0506b89536baf09a5ef54a5c48b67d
Gary <gdelacruz@neural.com.do>
2023-09-18 12:56:28 -0400

    End of Section 14

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket.service';
 import { Observable } from 'rxjs';
-import { IBasket } from '../shared/models/basket';
+import { IBasket, IBasketItem } from '../shared/models/basket';
 
 @Component({
   selector: 'app-basket',
@@ -15,6 +15,18 @@ export class BasketComponent implements OnInit{
   
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+  }
+
+  removeBasketItem(item: IBasketItem){
+    this.basketService.removeItemFromBasket(item);
+  }
+
+  incrementItemQuantity(item: IBasketItem){
+    this.basketService.incrementItemQuantity(item);
+  }
+
+  decrementItemQuantity(item: IBasketItem){
+    this.basketService.decrementItemQuantity(item);
   }
 
 }
--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -46,7 +46,48 @@ export class BasketService {
      const basket = this.getCurrentBasketValue() ?? this.createBasket();
      basket.items = this.addOrUpdateItem(basket.items, itemToAdd, quantity);
      this.setBasket(basket);
-}
+    }
+    
+    incrementItemQuantity(item: IBasketItem){
+      const basket = this.getCurrentBasketValue();
+      const foundItemIndex = basket.items.findIndex( x => x.id === item.id);
+      basket.items[foundItemIndex].quantity++;
+      this.setBasket(basket);
+      }
+
+    decrementItemQuantity(item: IBasketItem){
+      const basket = this.getCurrentBasketValue();
+      const foundItemIndex = basket.items.findIndex( x => x.id === item.id);
+      if(basket.items[foundItemIndex].quantity > 1){
+        basket.items[foundItemIndex].quantity--;
+        this.setBasket(basket);
+      } else {
+        this.removeItemFromBasket(item);
+      }
+    }
+
+    removeItemFromBasket(item: IBasketItem) {
+      const basket = this.getCurrentBasketValue();
+      if(basket.items.some( x => x.id === item.id)) {
+        basket.items = basket.items.filter( i => i.id !== item.id);
+        if(basket.items.length > 0){
+          this.setBasket(basket);
+        } else {
+          this.deleteBasket(basket);
+        }
+      }
+    }
+
+  deleteBasket(basket: IBasket) {
+    return this.http.delete(this.baseUrl + 'basket?id=' + basket.id).subscribe(() => {
+      this.basketSource.next(null);
+      this.basketTotalSource.next(null);
+      localStorage.removeItem('basket_id');
+    }, error => {
+      console.log(error);
+    });
+  }
+
 
   private calculateTotals() {
     const basket = this.getCurrentBasketValue();
