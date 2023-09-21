483393ef00c21224065b0c9d2ce4910496188af2
Gary <gdelacruz@neural.com.do>
2023-09-21 11:41:48 -0400

    End of Section 16

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -10,10 +10,12 @@ namespace API.Helpers
         public MappingProfiles()
         {
             CreateMap<Product, ProductToReturnDto>()
-            .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-            .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
-           .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+                .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
            CreateMap<Address, AddressDto>().ReverseMap();
+           CreateMap<CustomerBasketDto, CustomerBasket>();
+           CreateMap<BasketItemDto, BasketItem>();
         }
     }
 }
