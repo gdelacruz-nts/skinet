7118cdab0f156aa5bab713e63651ef11357a7c00
Gary <gdelacruz@neural.com.do>
2023-09-26 16:54:06 -0400

    Minor Changes Section 17

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
+  constructor(){}
+
+  ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
+  onSubmit(){
+    console.log(this.loginForm.value);
+  }
 }
